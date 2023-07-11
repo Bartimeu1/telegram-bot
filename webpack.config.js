20e8cde0a70eec6ec9a3c492a22ac2cb862c63ef
Ivan Sapego <sapego-ivan@list.ru>
2023-07-11 15:21:30 +0300

    fix: fixed AbortSignal error

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
+const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -18,4 +19,24 @@ export default {
       '@constants': path.resolve(__dirname, './src/constants'),
     },
   },
+};
+
+export default (env, argv) => {
+  if (argv.mode === 'production') {
+    config.mode = 'production';
+    config.devtool = 'source-map';
+    config.optimization = {
+      ...(config.optimization || {}),
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_fnames: /AbortSignal/,
+          },
+        }),
+      ],
+    };
+  }
+
+  return config;
 };
